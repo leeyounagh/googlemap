5e7e1cc0a15fdc3f4488deec1f2ca86f96f71903
youngah lee <dudgk0216@naver.com>
2022-06-29 20:27:39 +0900

    first commit

---
+++ src/Map.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Map = () => {
+    return (
+        <div style={{width:'700px',height:'600px',
+        border:'1px solid black',display:"flex",justifyContent:'center'}}>
+            
+        </div>
+    );
+};
+
+export default Map;

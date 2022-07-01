c61be0031f2223d338e510a74ed4867d45b1e04b
youngah lee <dudgk0216@naver.com>
2022-07-01 19:07:36 +0900

    마커구현

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,15 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import axios from 'axios';
 import { GoogleMap,useLoadScript,Marker } from '@react-google-maps/api';
+import { FaMapMarkerAlt } from 'react-icons/fa';
+const { MarkerWithLabel } = require("react-google-maps/lib/components/addons/MarkerWithLabel");
 
 const Home = () => {
+    useEffect(()=>{
+        <GoogleMap></GoogleMap>;
+
+<Map></Map>
+    },[])
 
     const {isLoaded} = useLoadScript({
         googleMapsApiKey:'AIzaSyBGRsNeYWBKmbw5-YWDwHVL8EOnXx-KTRc'
@@ -17,28 +24,42 @@ const Home = () => {
 
 };
 
-const location = [
-    {lat:Number(33.488663),lng:Number(126.48927)},
-    {lat:Number(33.2540922),lng:Number(126.5601945)},
-    {lat:Number(33.24957),lng:Number(126.56415)},
 
-]
    
 
 
 function Map(){
+    // const Marker = () =>{
+    //     <div>
+    //         <FaMapMarkerAlt width='60px'></FaMapMarkerAlt>
+    //     </div>
+    // }
+    const location = [
+        {id:0,lat:Number(33.488663),lng:Number(126.48927)},
+        {id:1,lat:Number(33.2540922),lng:Number(126.5601945)},
+        {id:2,lat:Number(33.24957),lng:Number(126.56415)},
+    
+    ]
 
-   axios.get('http://api.visitjeju.net/vsjApi/contents/searchList?apiKey=sbrr93ynwcggx6br&locale=kr')
-   .then(response=>console.log(response.data)
+//    axios.get('http://api.visitjeju.net/vsjApi/contents/searchList?apiKey=sbrr93ynwcggx6br&locale=kr')
+//    .then(response=>console.log(response.data)
   
-    )
+//     )
 
     const center =useMemo(()=>({lat:33.37212380975274,lng:126.53518867943278}),[]) 
     return(
         <div >
-             <GoogleMap zoom={9} center={center}
+             <GoogleMap zoom={10} center={center}
     mapContainerClassName='map-container'>
-        <Marker position={{lat:Number(33.2540922),lng:Number(126.5601945)}}></Marker>
+        {
+            location.map((item)=>(
+                <div key={item.id}>
+  <Marker position={{lat:item.lat,lng:item.lng}} id={item.id}></Marker>
+                </div>
+              
+            ))
+        }
+     
     </GoogleMap>
         </div>
     )
